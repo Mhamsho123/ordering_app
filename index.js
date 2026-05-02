c358abd47757ae8b6de39783eb7093ac8697923e
Mohamad Hamsho <hamsho1030@gmail.com>
2026-05-02 16:45:49 -0400

    Designed the css of the page

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
-]
+import { menuArray } from "./data.js";
+
+const orderEl = document.getElementById('order-items')
+
+
+
+
+
+function renderPage(){
+
+    let html = ''
+    menuArray.forEach(function({ name, ingredients, id, price, emoji }) {
+        html += `
+            <div class="menu-item-container">
+                <div class="emoji-container">
+                    <p class="emoji">${emoji}</p>
+                </div>
+                <div class="item-container">
+                    <h3>${name}</h3>
+                    <p>${ingredients.join(', ')}</p>
+                    <p>$${price}</p>
+                </div>
+                <div class="item-add-btn">
+                    <button data-id="${id}">+</button>
+                </div>
+            </div>
+        `
+    })
+
+    
+
+
+
+
+
+    orderEl.innerHTML = html
+}
+
+renderPage()
